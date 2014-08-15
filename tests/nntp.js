a32d7119c86f9eb7a7e4a617555e918413804c87
Robin van der Vleuten <robinvdvleuten@gmail.com>
2014-08-15 14:10:41 +0200

    Implemented the XZVER command for better performance. Added test cases for both xover() and xzver().

--- tests/nntp.js
+++ tests/nntp.js
@@ -1,6 +1,7 @@
 var assert = require('assert'),
     net = require('net'),
     async = require('async'),
+    zlib = require('zlib'),
     NNTP = require('../lib/nntp'),
     server;
 
@@ -21,7 +22,7 @@ function createServer(messages, callback) {
         message = messages.shift();
         assert.equal(message.request, data);
 
-        connection.write(message.response);
+        connection.write(new Buffer(message.response, 'binary'));
       });
     }
   });
@@ -133,6 +134,108 @@ describe('NNTP', function () {
           assert.notEqual(null, error);
           assert.equal(error.message, 'Password is required');
           done();
+        });
+      });
+    });
+  });
+
+  describe('#xover()', function () {
+    it('should return messages as array', function (done) {
+      var messages = [
+        { request: 'XOVER 1-1\r\n', response: '224 compressed data follows\r\n123456789\tRe: Are you checking out NNTP?\trobinvdvleuten@example.com (\"Robin van der Vleuten\")\tSat,3 Aug 2013 13:19:22 -0000\t<nntp123456789@nntp>\t<nntp987654321@nntp>\t321\t123\tXref: nntp:123456789\r\n.\r\n' }
+      ];
+
+      createServer(messages, function () {
+        var nntp = new NNTP({host: 'localhost', port: 5000, username: 'user'});
+
+        async.waterfall([
+          function (callback) {
+            nntp.connect(callback);
+          },
+          function (response, callback) {
+            var format = {
+              subject: false,
+              from: false,
+              date: false,
+              'message-id': false,
+              references: false,
+              bytes: false,
+              lines: false,
+              xref: true
+            };
+
+            nntp.xover('1-1', format, callback);
+          },
+          function (messages, callback) {
+            assert.equal(1, messages.length);
+
+            var message = messages.shift();
+            assert.equal('123456789', message.number);
+            assert.equal('Re: Are you checking out NNTP?', message.subject);
+            assert.equal('robinvdvleuten@example.com ("Robin van der Vleuten")', message.from);
+            assert.equal('Sat,3 Aug 2013 13:19:22 -0000', message.date);
+            assert.equal('<nntp123456789@nntp>', message['message-id']);
+            assert.equal('<nntp987654321@nntp>', message.references);
+            assert.equal('321', message.bytes);
+            assert.equal('123', message.lines);
+            assert.equal('nntp:123456789', message.xref);
+
+            callback();
+          }
+        ], function () {
+          done();
+        });
+      });
+    });
+  });
+
+  describe('#xzver()', function () {
+    it('should correctly inflate received messages and return messages as array', function (done) {
+      zlib.deflate('123456789\tRe: Are you checking out NNTP?\trobinvdvleuten@example.com (\"Robin van der Vleuten\")\tSat,3 Aug 2013 13:19:22 -0000\t<nntp123456789@nntp>\t<nntp987654321@nntp>\t321\t123\tXref: nntp:123456789\r\n.\r\n', function (error, result) {
+        var messages = [
+          { request: 'XZVER 1-1\r\n', response: '224 compressed data follows\r\n' + result.toString('binary') }
+        ];
+
+        createServer(messages, function () {
+          var nntp = new NNTP({host: 'localhost', port: 5000, username: 'user'});
+
+          async.waterfall([
+            function (callback) {
+              nntp.connect(callback);
+            },
+            function (response, callback) {
+              var format = {
+                subject: false,
+                from: false,
+                date: false,
+                'message-id': false,
+                references: false,
+                bytes: false,
+                lines: false,
+                xref: true
+              };
+
+              nntp.xzver('1-1', format, callback);
+            },
+            function (messages, callback) {
+              assert.equal(1, messages.length);
+
+              var message = messages.shift();
+              assert.equal('123456789', message.number);
+              assert.equal('Re: Are you checking out NNTP?', message.subject);
+              assert.equal('robinvdvleuten@example.com ("Robin van der Vleuten")', message.from);
+              assert.equal('Sat,3 Aug 2013 13:19:22 -0000', message.date);
+              assert.equal('<nntp123456789@nntp>', message['message-id']);
+              assert.equal('<nntp987654321@nntp>', message.references);
+              assert.equal('321', message.bytes);
+              assert.equal('123', message.lines);
+              assert.equal('nntp:123456789', message.xref);
+
+              callback();
+            }
+          ], function () {
+            done();
+          });
         });
       });
     });
